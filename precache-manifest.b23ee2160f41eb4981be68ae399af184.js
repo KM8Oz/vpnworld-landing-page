fc387f58dd3352c5eb4cd5e96dadbbf3f33deb5f
km8oz <ouazmir@gmail.com>
2022-05-01 16:09:50 +0300

    Updates

---
+++ precache-manifest.b23ee2160f41eb4981be68ae399af184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c88401a80e15766c9b5e",
+    "url": "/vpnworld-landing-page/static/css/main.2b9e6e1a.chunk.css"
+  },
+  {
+    "revision": "c88401a80e15766c9b5e",
+    "url": "/vpnworld-landing-page/static/js/main.c88401a8.chunk.js"
+  },
+  {
+    "revision": "7cf214d6c915511e8fbb",
+    "url": "/vpnworld-landing-page/static/js/1.7cf214d6.chunk.js"
+  },
+  {
+    "revision": "74f1d9f8b6ef6fc9df0e",
+    "url": "/vpnworld-landing-page/static/js/2.74f1d9f8.chunk.js"
+  },
+  {
+    "revision": "f7024e372ef3a956a3ea",
+    "url": "/vpnworld-landing-page/static/js/runtime~main.f7024e37.js"
+  },
+  {
+    "revision": "8691753d5ecfd9a2c069fe5a3c0d213b",
+    "url": "/vpnworld-landing-page/index.html"
+  }
+];
