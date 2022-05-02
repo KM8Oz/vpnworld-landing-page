0744527b536c2ed239f41dde3f9542254cd28bf9
km8oz <ouazmir@gmail.com>
2022-05-02 15:31:04 +0300

    Updates

---
+++ precache-manifest.0cf48745dded4c83780f6d4760275c9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d10bd516f14b1215444",
+    "url": "/static/css/main.3504517d.chunk.css"
+  },
+  {
+    "revision": "2d10bd516f14b1215444",
+    "url": "/static/js/main.2d10bd51.chunk.js"
+  },
+  {
+    "revision": "56dbc343fe2dac40479d",
+    "url": "/static/js/1.56dbc343.chunk.js"
+  },
+  {
+    "revision": "740f9319a7eb11f9cc16",
+    "url": "/static/js/2.740f9319.chunk.js"
+  },
+  {
+    "revision": "cfbbace2586378dd3871",
+    "url": "/static/js/runtime~main.cfbbace2.js"
+  },
+  {
+    "revision": "63dae2d5e29185729e0d033d4cebc766",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vpnworld-landing-page/precache-manifest.6b9281e0f1139309070785af32c2eff2.js"
+  "/precache-manifest.0cf48745dded4c83780f6d4760275c9c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vpnworld-landing-page/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
